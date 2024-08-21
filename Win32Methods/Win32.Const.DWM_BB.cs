06c4130f0d0cdefc7d80715fb242a49809634af1
yi xin <3077501050@qq.com>
2024-08-21 23:39:00 +0800

    删除多余的 Win32Methods 项目文件夹

--- Win32Methods/Win32.Const.DWM_BB.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Win32.Constants;
-
-public static class DwmBBFlags
-{
-    public const uint DWM_BB_ENABLE = 0x00000001;
-    public const uint DWM_BB_BLURREGION = 0x00000002;
-    public const uint DWM_BB_TRANSITIONONMAXIMIZED = 0x00000004;
-}
--- Win32Methods/Win32.Const.GW.01.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Win32.Constants;
-
-public static partial class GetWindowCmd
-{
-    public const int GW_HWNDPREV = 3;
-    public const int GW_HWNDNEXT = 2;
-    public const int GW_OWNER = 4;
-}
